dc467a7b63590117c1f3bd26ed1533073f96be10
proelias7 <proelias7@live.com>
2024-06-27 11:04:36 -0400

    att

--- src/app.js
+++ src/app.js
@@ -2,10 +2,7 @@ import fs from 'fs'
 import cron from'node-cron'
 import dir from 'path'
 import mysqldump from 'mysqldump';
-import express from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-const app = express()
+
 const config = await JSON.parse(fs.readFileSync('./config.json'));
 
 process.on('uncaughtException',async (error,origin) => {
@@ -22,67 +19,6 @@ process.on('uncaughtExceptionMonitor', (error, origin) => {
     const data = `Tipo: uncaughtExceptionMonitor \nOrigen: ${origin}\nError: ${error}`
     console.log(data)
 });
-
-const initServer = async () => {
-    try{
-        app.use(bodyParser.urlencoded({extended:false}))
-        app.use(express.json())
-        app.use(cors())
-        const router = express.Router()
-        const authenticateAPIKey = (req, res, next) => {
-            const providedKey = req.headers['x-api-key'];
-        
-            if (providedKey && providedKey === config.apiKey) {
-                next();
-            } else {
-                return res.status(401).json({ error: 'Acesso não autorizado' });
-            }
-        };
-        
-        router.get('/backups',authenticateAPIKey, (req, res) => {
-            let backups = {};
-            const backupDir = './storage';
-        
-            fs.readdir(backupDir, (err, databases) => {
-                if (err) {
-                    return res.status(500).json({ error: 'Erro ao carregar backups' });
-                }
-        
-                databases.forEach(database => {
-                    const pathDB = dir.join(backupDir, database);
-                    try {
-                        const files = fs.readdirSync(pathDB);
-                        backups[database] = files.map(file => {
-                            const [day, month, year, hour, minute] = file.split('.sql')[0].split('-');
-                            return `${day}-${month}-${year} ${hour}:${minute}`;
-                        });
-                    } catch (error) {
-                        console.error(`Erro ao ler backup ${database}:`, error);
-                    }
-                });
-        
-        
-                res.json(backups);
-            });
-        });
-
-        app.use(router)
-
-        const server = app.listen(config.port, () =>{
-            console.log('Servidor iniciado na porta ' + config.port)
-        })
-
-        server.on('error', (error) => {
-            if (error.code === 'EADDRINUSE') {
-                log('negado', `Servidor [ error ] => porta ${config.port} já está em uso.`)
-            } else {
-                log('negado', 'Servidor [ error ] => Ocorreu um erro ao iniciar o servidor:');
-            }
-        });
-    } catch (error) {
-        console.log(error);
-    }
-}
 
 try{
     cron.schedule(config.cron, async() => {
@@ -139,7 +75,6 @@ try{
         });
     });
     console.log('Aplicação iniciada');
-    initServer();
 } catch (error) {
     console.log(error);
 }
